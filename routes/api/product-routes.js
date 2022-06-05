b201ea99d3a05f07342bd376edaf8d19837ec928
Zekun Chen <zekunchen97@gmail.com>
2022-06-05 20:46:20 +0930

    testing dbs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -70,39 +70,37 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(
-    {
+    Product.create({
       product_name: req.body.product_name,
       price: req.body.price,
       stock: req.body.stock,
       category_id: req.body.category_id,
       tagIds: req.body.tagIds
-      }
-
-  )
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+      })
+      .then((product) => {
+        // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+        if (req.body.tagIds.length) {
+          const productTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+              product_id: product.id,
+              tag_id,
+            };
+          });
+          return ProductTag.bulkCreate(productTagIdArr);
+        }
+        // if no product tags, just respond
+        res.status(200).json(product);
+      })
+      .then((productTagIds) => res.status(200).json(productTagIds))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
 });
 
 // update product
 router.put('/:id', (req, res) => {
+  // update product data
   // update product data
   Product.update(req.body, {
     where: {
@@ -143,8 +141,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
-
 router.delete('/:id', (req, res) => {
+  // delete one product by its `id` value
   Product.destroy({
     where: {
       id: req.params.id
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("Wrong Route!")
+});
 
 module.exports = router;
